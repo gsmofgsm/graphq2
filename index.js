03d722b49ed5b7bc84f48bb4a8edf06cbb481c0d
Qing Ma <qing.ma@isatis-health.nl>
2019-08-02 17:41:16 +0200

    schema

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
+    users: [User]
+    user(name: String): User
     me2: User
   }
 
   type User {
     id: ID!
+    name: String
     username: String
   }
 `;
@@ -15,9 +18,35 @@ const resolvers = {
   Query: {
     me2() {
       return { id: "2", username: "@gsm" }
+    },
+    user(parent, args) {
+      const { name } = args;
+      return users.find((user) => user.name === name);
+    },
+    users() {
+      return users;
     }
   }
 };
+
+const users = [
+  {
+    name: 'J.K. Rowling',
+    username: '@J.K. Rowling'
+  },
+  {
+    name: 'Michael Crichton',
+    username: '@Michael Crichton'
+  },
+  {
+    name: 'tom',
+    username: '@tom'
+  },
+  {
+    name: 'jerry',
+    username: '@jerry'
+  },
+]
 
 const server = new ApolloServer({
   typeDefs,
